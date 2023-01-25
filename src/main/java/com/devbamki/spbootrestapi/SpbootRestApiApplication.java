074d4cc38ce39e9f40f6f96ffd24fa78b861b970
qjarl5678 <qjarl5678@nate.com>
2023-01-25 12:37:53 +0900

    chore: add jpa

---
+++ src/main/java/com/devbamki/spbootrestapi/SpbootRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.devbamki.spbootrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbootRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbootRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devbamki/spbootrestapi/SpbootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devbamki.spbootrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbootRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
