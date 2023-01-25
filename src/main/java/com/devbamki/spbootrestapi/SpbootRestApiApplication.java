c07408bd9d878916cef0a92d4f85bcc3d1b491ae
qjarl5678 <qjarl5678@nate.com>
2023-01-25 15:32:53 +0900

    chore: add jdbc config

--- src/main/java/com/devbamki/spbootrestapi/SpbootRestApiApplication.java
+++ src/main/java/com/devbamki/spbootrestapi/SpbootRestApiApplication.java
@@ -2,8 +2,10 @@ package com.devbamki.spbootrestapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = {"classpath:jdbc.properties"})
 public class SpbootRestApiApplication {
 
 	public static void main(String[] args) {
