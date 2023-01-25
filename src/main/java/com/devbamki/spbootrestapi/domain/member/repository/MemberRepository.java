373f46ad42b1617ef1df9583dfa9a7e59b0f2bcb
qjarl5678 <qjarl5678@nate.com>
2023-01-25 17:19:36 +0900

    feat: add member repo

---
+++ src/main/java/com/devbamki/spbootrestapi/domain/member/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.devbamki.spbootrestapi.domain.member.repository;
+
+import com.devbamki.spbootrestapi.domain.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUserName(String userName);
+    boolean exitsByUserName(String userName);
+}
---
+++ src/test/java/com/devbamki/spbootrestapi/domain/member/repository/MemberRepositoryTest.java
@@ -0,0 +1,25 @@
+package com.devbamki.spbootrestapi.domain.member.repository;
+
+import org.junit.jupiter.api.AfterEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    EntityManager entityManager;
+
+    @AfterEach
+    private void after(){
+        entityManager.clear();
+    }
+
+}
