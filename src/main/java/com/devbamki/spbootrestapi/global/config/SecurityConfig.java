378f033d9d102a14afb67af7f609ecd019a95e6d
qjarl5678 <qjarl5678@nate.com>
2023-01-29 14:56:00 +0900

    feat: Security Config change

--- src/main/java/com/devbamki/spbootrestapi/global/config/SecurityConfig.java
+++ src/main/java/com/devbamki/spbootrestapi/global/config/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.devbamki.spbootrestapi.global.config;
 
+import com.devbamki.spbootrestapi.domain.member.service.LoginService;
 import com.devbamki.spbootrestapi.global.login.filter.JsonUserIdPasswordAuthenticationFilter;
+import com.devbamki.spbootrestapi.global.login.handler.LoginFailureHandler;
+import com.devbamki.spbootrestapi.global.login.handler.LoginSuccessJWTProvideHandler;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,13 +15,16 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final LoginService loginService;
     private final ObjectMapper objectMapper;
 
-    public SecurityConfig(ObjectMapper objectMapper) {
+    public SecurityConfig(LoginService loginService, ObjectMapper objectMapper) {
+        this.loginService = loginService;
         this.objectMapper = objectMapper;
     }
 
@@ -34,6 +40,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/login", "signUp", "/")
                 .permitAll()
                 .anyRequest().authenticated();
+
+        http.addFilterAfter(jsonUserIdPasswordLoginFilter(), LogoutFilter.class);
     }
 
     @Bean
@@ -49,9 +57,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
+    public LoginSuccessJWTProvideHandler loginSuccessJWTProvideHandler(){
+        return new LoginSuccessJWTProvideHandler();
+    }
+
+    @Bean
+    public LoginFailureHandler loginFailureHandler(){
+        return new LoginFailureHandler();
+    }
+
+    @Bean
     public JsonUserIdPasswordAuthenticationFilter jsonUserIdPasswordLoginFilter(){
         JsonUserIdPasswordAuthenticationFilter jsonUserIdPasswordLoginFilter = new JsonUserIdPasswordAuthenticationFilter(objectMapper);
         jsonUserIdPasswordLoginFilter().setAuthenticationManager(authenticationManager());
+        jsonUserIdPasswordLoginFilter.setAuthenticationSuccessHandler(loginSuccessJWTProvideHandler());
+        jsonUserIdPasswordLoginFilter.setAuthenticationFailureHandler(loginFailureHandler());
 
         return jsonUserIdPasswordLoginFilter;
     }
