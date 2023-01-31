cc08bc3f34192702dcf72d5be068b52d8791b5b7
qjarl5678 <qjarl5678@nate.com>
2023-01-31 18:15:34 +0900

    fix: Member Repo에 Refresh Token을 통해 조회하는 메소드 추가

--- src/main/java/com/devbamki/spbootrestapi/domain/member/repository/MemberRepository.java
+++ src/main/java/com/devbamki/spbootrestapi/domain/member/repository/MemberRepository.java
@@ -9,4 +9,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByUserId(String userId);
     boolean existsByUserId(String userId);
 
+    Optional<Member> findByRefreshToken(String refreshToken);
+
 }
